c0494adbd4c00d32354500e93572e2c1c101a522
eduardaarosaa <eduardacirina@gmail.com>
2019-06-19 11:39:46 -0300

    Pronto (:

--- script/script.js
+++ script/script.js
@@ -1,41 +1,19 @@
-function contar() {
-
-    var inicio = document.getElementById("inicio");
-    var fim = document.getElementById("fim");
-    var passo = document.getElementById("passo");
-    var res = document.getElementById("res");
-    var c;
-
-    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert("falta dados");
-        res.innerHTML = "Impossivel contar."
+function tabuada() {
+    let num = document.getElementById("txt1");
+    let tabuada = document.getElementById('seltab');
+    if (num.value.length == 0) {
+        window.alert("Por favor, digite um número!");
     } else {
-        res.innerHTML = "Contando...<br>";
-        var i = Number(inicio.value);
-        var f = Number(fim.value);
-        var p = Number(passo.value);
-
-        if(p <= 0){
-            window.alert("Passo inválido!!! Será considerado o PASSO 1");
-            p = 1;
+        let n = Number(num.value);
+        let c = 1;
+        //Limpa para mostrar outra tabuada
+        tabuada.innerHTML = "";
+        while(c<=10){
+            let item = document.createElement('option');
+            item.text = `${n} x ${c} = ${n*c} `;
+            tabuada.appendChild(item);
+            c++;
         }
-
-        if (i < f) {
-
-            for (c = i; c <= f; c += p) {
-
-                res.innerHTML += `${c}  \u{1F603}	`;
-            }
-            // Unicode no JS é u\
-        } else {
-            for (c = i; c >= f; c -= p) {
-
-                res.innerHTML += `${c} \u{1F603}	`;
-
-            }
-        }
-        
-        res.innerHTML += `${c} \u{1F603}	`;
 
     }
 
